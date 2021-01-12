6ab00dd01a69fe90e0a7ea0c9c43e71b5603fc9b
Tatiana Solomatina <lisichka-tanya@mail.ru>
2021-01-12 12:52:59 +0300

    HW11 remove class Runner in DeadLock

--- src/com/urise/webapp/Deadlock.java
+++ src/com/urise/webapp/Deadlock.java
@@ -5,38 +5,23 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class Deadlock {
     public static void main(String[] args) {
-        Runner runner = new Runner();
-        Thread thread1 = new Thread(runner::firstThread);
-        Thread thread2 = new Thread(runner::secondThread);
+        final Lock lock1 = new ReentrantLock();
+        final Lock lock2 = new ReentrantLock();
+        Thread thread1 = new Thread(() -> runDeadlock(lock1, lock2));
+        Thread thread2 = new Thread(() -> runDeadlock(lock2, lock1));
 
         thread1.start();
         thread2.start();
     }
-}
 
-class Runner {
-    private final Lock lock1 = new ReentrantLock();
-    private final Lock lock2 = new ReentrantLock();
-
-    public void firstThread() {
-        for (int i = 0; i < 10_00; i++) {
-            synchronized (lock1) {
-                System.out.println("Hold lock1, wait lock2");
-                synchronized (lock2) {
-                    System.out.println("Hold lock1, hold lock2");
-                }
-            }
-        }
-    }
-
-    public void secondThread() {
-        for (int i = 0; i < 10_00; i++) {
-            synchronized (lock2) {
-                System.out.println("Hold lock2, wait lock1");
-                synchronized (lock1) {
-                    System.out.println("Hold lock2, hold lock1");
-                }
+    static void runDeadlock(Lock firstLock, Lock secondLock) {
+        synchronized (firstLock) {
+            System.out.println("Hold " + Thread.currentThread().getName() + ", wait " + secondLock);
+            synchronized (secondLock) {
+                System.out.println("Hold both locks");
             }
         }
     }
 }
+
+
