1b2f9b9589a1e34a402e221a8f766a629e81b0c8
Tatiana Solomatina <lisichka-tanya@mail.ru>
2021-01-13 17:17:34 +0300

    HW11 effective deadlock

--- src/com/urise/webapp/Deadlock.java
+++ src/com/urise/webapp/Deadlock.java
@@ -1,26 +1,28 @@
 package com.urise.webapp;
 
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class Deadlock {
     public static void main(String[] args) {
-        final Lock lock1 = new ReentrantLock();
-        final Lock lock2 = new ReentrantLock();
-        Thread thread1 = new Thread(() -> runDeadlock(lock1, lock2));
-        Thread thread2 = new Thread(() -> runDeadlock(lock2, lock1));
-
-        thread1.start();
-        thread2.start();
+        final String lock1 = "lock1";
+        final String lock2 = "lock2";
+        runDeadlock(lock1, lock2);
+        runDeadlock(lock2, lock1);
     }
 
-    static void runDeadlock(Lock firstLock, Lock secondLock) {
-        synchronized (firstLock) {
-            System.out.println("Hold " + Thread.currentThread().getName() + ", wait " + secondLock);
-            synchronized (secondLock) {
-                System.out.println("Hold both locks");
+    static void runDeadlock(String firstLock, String secondLock) {
+        new Thread(() -> {
+            synchronized (firstLock) {
+                System.out.println(Thread.currentThread().getName() + " hold " + firstLock + " and wait " + secondLock);
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (secondLock) {
+                    System.out.println("Hold both locks");
+                }
             }
-        }
+        }).start();
     }
 }
 
