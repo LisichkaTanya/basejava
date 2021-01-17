46373748f469b40ead5c9ac84cc80a3e3aeb9ea5
Tatiana Solomatina <lisichka-tanya@mail.ru>
2021-01-17 16:20:21 +0300

    lesson 12 Concurrency

--- src/com/urise/webapp/MainConcurrency.java
+++ src/com/urise/webapp/MainConcurrency.java
@@ -1,13 +1,34 @@
 package com.urise.webapp;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class MainConcurrency {
     private static int counter;
+    private final AtomicInteger atomicCounter = new AtomicInteger();
     //Объект, к которому мы будем ставить потоки в очередь для синхронизации
-    private static final Object LOCK = new Object();
+//    private static final Object LOCK = new Object();
     public static final int THREADS_NUMBER = 10000;
+    private static final ReentrantReadWriteLock reentrantReadWriteLock = new ReentrantReadWriteLock();
+    private static final Lock WRITE_LOCK = reentrantReadWriteLock.writeLock();
+    private static final Lock READ_LOCK = reentrantReadWriteLock.readLock();
+
+//        private static final ThreadLocal<SimpleDateFormat> threadLocal = new ThreadLocal<SimpleDateFormat>() {
+//        @Override
+//        protected SimpleDateFormat initialValue() {
+//            return new SimpleDateFormat();
+//        }
+//    };
+//    можно написать через лямбду:
+    private static final ThreadLocal<SimpleDateFormat> threadLocal =
+            ThreadLocal.withInitial(() -> new SimpleDateFormat("dd.MM.yyyy HH:mm:ss"));
+//    А лучше использовать потокобезопасный DateTimeFormatter Java 8 Time API:
+//    private static final DateTimeFormatter threadLocal = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println(Thread.currentThread().getName());
@@ -17,6 +38,7 @@ public class MainConcurrency {
             @Override
             public void run() {
                 System.out.println(getName() + ", " + getState());
+//                throw new IllegalStateException();
             }
         };
         thread0.start();
@@ -29,34 +51,60 @@ public class MainConcurrency {
         System.out.println(thread0.getState());
 
         final MainConcurrency mainConcurrency = new MainConcurrency();
-        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
+
+        //вместо создания листа с Thread-ами и join-иться к ним мы можем использовать CountDownLatch
+        //latch - это замок с обратным отсчетом
+//        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
+//        for (int i = 0; i < THREADS_NUMBER; i++) {
+//            Thread thread = new Thread(() -> {
+//                for (int j = 0; j < 100; j++) {
+//                    mainConcurrency.increment();
+//                }
+//            });
+//            thread.start();
+//            threads.add(thread);
+//        }
+//
+//        //Поставим ожидание, пока наш поток завершит заполнять counter, и только потом выведем на экран
+////        Thread.sleep(500);
+//        threads.forEach(t -> {
+//            try {
+//                t.join();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        });
+
+        CountDownLatch latch = new CountDownLatch(THREADS_NUMBER);
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        //Если мы подгружаем html страницу с картинками, чтобы картинки показывались по мере подгружения
+        //можно использовать CompletionService, это интерфейс и он позволяет брать первую законченную задачу(task)
+        //то есть покажется первая загруженная картинка
+//        CompletionService completionService = new ExecutorCompletionService(executorService);
+//        completionService.poll(); //возвращает Future
         for (int i = 0; i < THREADS_NUMBER; i++) {
-            Thread thread = new Thread(() -> {
+//            Thread thread = new Thread(() -> {
+            Future<Integer> future = executorService.submit(() -> {
                 for (int j = 0; j < 100; j++) {
                     mainConcurrency.increment();
+                    System.out.println(threadLocal);
                 }
+                latch.countDown();
+                return 5;
             });
-            thread.start();
-            threads.add(thread);
+//            thread.start();
         }
-
-        //Поставим ожидание, пока наш поток завершит заполнять counter, и только потом выведем на экран
-//        Thread.sleep(500);
-        threads.forEach(t -> {
-            try {
-                t.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-        System.out.println(counter);
+        latch.await();
+        executorService.shutdown();
+        System.out.println(mainConcurrency.atomicCounter);
     }
 
     //Если мы пишем synchronized в сигнатуре статического метода (перед void), то это значит мы встаем в очередь(waitSet)
     // к объекту класса, в котором метод, то есть тоже самое, что synchronized(MainConcurrency.class) {}
-    private synchronized void increment() {
+    private void increment() {
 //        synchronized(LOCK) {
-        counter++;
+//        counter++;
 //        }
+        atomicCounter.incrementAndGet();
     }
 }
