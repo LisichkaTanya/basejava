58b7c07c102c7bf575ca7188db7644754c51c479
Tatiana Solomatina <lisichka-tanya@mail.ru>
2021-01-22 20:51:25 +0300

    HW12 refactor methods 'minValue' and 'oddOrEven'

--- src/com/urise/webapp/MainStream.java
+++ src/com/urise/webapp/MainStream.java
@@ -11,11 +11,8 @@ public class MainStream {
         int result2 = minValue(new int[]{9, 8});
         System.out.println(result2);
 
-        List<Integer> list1 = new ArrayList<>(Arrays.asList(2, 4, 5, 6, 7));
-        List<Integer> list2 = new ArrayList<>(Arrays.asList(1, 4, 5, 6, 7));
+        List<Integer> list1 = new ArrayList<>(Arrays.asList(1, 4, 5, 6, 7));
         System.out.println(oddOrEven(list1));
-        System.out.println(oddOrEven(list2));
-
     }
 
     /*
@@ -25,8 +22,10 @@ public class MainStream {
      * Например {1,2,3,3,2,3} вернет 123, а {9,8} вернет 89
      */
     static int minValue(int[] values) {
-//        return Arrays.stream(values).boxed().collect(Collectors.toSet()).stream().reduce(0, (x, y) -> Integer.parseInt(x + "" + y));
-        return Arrays.stream(values).distinct().sorted().reduce(0, (x, y) -> Integer.parseInt(x + "" + y));
+        return Arrays.stream(values)
+                .distinct()
+                .sorted()
+                .reduce(0, (x, y) -> Integer.parseInt(x + "" + y));
     }
 
     /*
@@ -35,15 +34,11 @@ public class MainStream {
      *  Optional - решение в один стрим.
      */
     static List<Integer> oddOrEven(List<Integer> integers) {
-        return integers.stream().filter(x -> (x % 2) != ((integers.stream().reduce(0, Integer::sum)) % 2))
+        int sum = integers.stream()
+                .reduce(0, Integer::sum);
+
+        return integers.stream()
+                .filter(x -> (x % 2) != (sum % 2))
                 .collect(Collectors.toList());
     }
-
-    /*
-    Еще один вариант решения через Map
-     */
-//    public static List<Integer> oddOrEven2(List<Integer> integers) {
-//        Map<Boolean, List<Integer>> oddsAndEvens = integers.stream().collect(Collectors.partitioningBy(i -> i % 2 == 0));
-//        return oddsAndEvens.get(false).size() % 2 == 0 ? oddsAndEvens.get(false) : oddsAndEvens.get(true);
-//    }
 }
