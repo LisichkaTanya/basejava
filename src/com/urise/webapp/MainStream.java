7703a8e2017dc7d57f3667bbf213a7d3286d4cd8
Tatiana Solomatina <lisichka-tanya@mail.ru>
2021-01-22 09:38:33 +0300

    HW12 methods 'minValue' and 'oddOrEven'

--- src/com/urise/webapp/MainStream.java
+++ src/com/urise/webapp/MainStream.java
@@ -5,20 +5,17 @@ import java.util.stream.Collectors;
 
 public class MainStream {
     public static void main(String[] args) {
-        int result1 = minValue(new int[]{1, 2, 3, 3, 2, 3});
+        int result1 = minValue(new int[]{2, 1, 3, 3, 2, 3});
         System.out.println(result1);
 
         int result2 = minValue(new int[]{9, 8});
         System.out.println(result2);
 
-        List<Integer> list1 = new ArrayList<>();
-        list1.add(2);
-        list1.add(4);
-        list1.add(5);
-        list1.add(6);
-        list1.add(7);
-
+        List<Integer> list1 = new ArrayList<>(Arrays.asList(2, 4, 5, 6, 7));
+        List<Integer> list2 = new ArrayList<>(Arrays.asList(1, 4, 5, 6, 7));
         System.out.println(oddOrEven(list1));
+        System.out.println(oddOrEven(list2));
+
     }
 
     /*
@@ -28,14 +25,8 @@ public class MainStream {
      * Например {1,2,3,3,2,3} вернет 123, а {9,8} вернет 89
      */
     static int minValue(int[] values) {
-        Set<Integer> set = Arrays.stream(values).boxed().collect(Collectors.toSet());
-        double result = 0;
-        int power = set.size();
-        for (Integer integer : set) {
-            power--;
-            result += integer * (Math.pow(10, power));
-        }
-        return (int) result;
+//        return Arrays.stream(values).boxed().collect(Collectors.toSet()).stream().reduce(0, (x, y) -> Integer.parseInt(x + "" + y));
+        return Arrays.stream(values).distinct().sorted().reduce(0, (x, y) -> Integer.parseInt(x + "" + y));
     }
 
     /*
@@ -44,10 +35,15 @@ public class MainStream {
      *  Optional - решение в один стрим.
      */
     static List<Integer> oddOrEven(List<Integer> integers) {
-        if (integers.stream().reduce(Integer::sum).get() % 2 == 1) {
-            return integers.stream().filter(integer -> integer%2 != 1).collect(Collectors.toList());
-        } else {
-            return integers.stream().filter(integer -> integer%2 == 1).collect(Collectors.toList());
-        }
+        return integers.stream().filter(x -> (x % 2) != ((integers.stream().reduce(0, Integer::sum)) % 2))
+                .collect(Collectors.toList());
     }
+
+    /*
+    Еще один вариант решения через Map
+     */
+//    public static List<Integer> oddOrEven2(List<Integer> integers) {
+//        Map<Boolean, List<Integer>> oddsAndEvens = integers.stream().collect(Collectors.partitioningBy(i -> i % 2 == 0));
+//        return oddsAndEvens.get(false).size() % 2 == 0 ? oddsAndEvens.get(false) : oddsAndEvens.get(true);
+//    }
 }
